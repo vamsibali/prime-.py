9fb193699ebcfb3c3f8c8271a56eddf4869f44c1
vamsibali <165309099+vamsibali@users.noreply.github.com>
2024-04-01 13:00:56 +0530

    Add files via upload

---
+++ prime().py
@@ -0,0 +1,19 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+# Example usage:
+num=23
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
